c95569fceb05d52e504695fafea8d375b26ba116
Michelle Ranly <meranly8@gmail.com>
2020-09-13 18:16:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,6 +4,11 @@
 #   squared_nums
 # end
 
+def square_array(array)
+  squared_nums = []
+  array.each{|number| squared_nums.push!(number**2)
+end
+
 # def square_array(array)
 #   array.collect{|number| number**2}
 # end
